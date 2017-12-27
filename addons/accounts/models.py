01981cd1afe57c625d91f0c2750270a3e4c28e56
atul <jain.atul43@gmail.com>
2017-12-27 19:44:05 +0530

    auto incremental user_id

--- addons/accounts/models.py
+++ addons/accounts/models.py
@@ -12,6 +12,16 @@ import base64, uuid
 # Create your models here.
 User._meta.local_fields[4].__dict__['_unique'] = True
 
+def increment_user_id():
+    last_user_id = Profile.objects.all().order_by('user_auto_id').last()
+    if not last_user_id.user_auto_id:
+         return 'AVI000001'
+    user_id_no = last_user_id.user_auto_id
+    user_id_int = int(user_id_no.split('AVI')[-1])
+    new_user_id_int = user_id_int + 1
+    new_user_id_no = 'AVI' + str(new_user_id_int)
+    return new_user_id_no
+    
 class Profile(models.Model):
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	placement_type = (
@@ -24,6 +34,7 @@ class Profile(models.Model):
 		('C', 'Confirmed'),
 		('NC', 'Non-Confirmed')
 		)
+	user_auto_id = models.CharField(max_length=500, default=increment_user_id, null=True, blank=True)
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	referal_code = models.CharField(max_length=20, blank=True)
 	# sponser_id =models.OneToOneField(User, null=True, related_name='+')
@@ -54,6 +65,7 @@ class Profile(models.Model):
 		elif not self.my_referal_code:
 			self.my_referal_code = self.generate_referal_code()
 		return super(Profile, self).save(*args, **kwargs)
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
