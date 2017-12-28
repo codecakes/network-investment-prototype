1cf161f697eee27637418fd171d8115afc822ff4
Jasvin Singh <jasvinsinghk@gmail.com>
2017-12-28 15:12:05 +0530

    fix dom not find problem.

--- static/js/network.js
+++ static/js/network.js
@@ -1,24 +1,27 @@
 $(function(){
-    $.ajax({
-        type: "POST",
-        url: "/network",
-        success: function(prod_detail) {
-            var data = prod_detail//JSON.parse(prod_detail)
-            console.log(data,"fkgnkfng")
-            var networkData = chart_config = {
-                chart: {
-                    container: "#networkTree",
-                    
-                    connectors: {
-                        type: 'step'
+    var treeDom = $("#networkTree");
+    if(treeDom.length){
+        $.ajax({
+            type: "POST",
+            url: "/network",
+            success: function(prod_detail) {
+                var data = prod_detail
+                var networkData = chart_config = {
+                    chart: {
+                        container: "#networkTree",
+                        
+                        connectors: {
+                            type: 'step'
+                        },
+                        node: {
+                            HTMLclass: 'nodeExample1'
+                        }
                     },
-                    node: {
-                        HTMLclass: 'nodeExample1'
-                    }
-                },
-                nodeStructure:JSON.parse(data)
-            } 
-            new Treant( chart_config );
-        }
-    });
+                    nodeStructure:JSON.parse(data)
+                } 
+                var networkTree = new Treant( chart_config );
+            }
+        });
+        
+    }
 })
