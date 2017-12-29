93a859a826f96018fe2b742c6377661efc841567
atul <jain.atul43@gmail.com>
2017-12-29 14:16:25 +0530

    models temp comments

--- addons/accounts/models.py
+++ addons/accounts/models.py
@@ -13,15 +13,15 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 User._meta.local_fields[4].__dict__['_unique'] = True
 
-def increment_user_id():
-    last_user_id = Profile.objects.all().order_by('user_auto_id').last()
-    if not last_user_id.user_auto_id:
-         return 'AVI000001'
-    user_id_no = last_user_id.user_auto_id
-    user_id_int = int(user_id_no.split('AVI')[-1])
-    new_user_id_int = user_id_int + 1
-    new_user_id_no = 'AVI' + str(new_user_id_int)
-    return new_user_id_no
+# def increment_user_id():
+#     last_user_id = Profile.objects.all().order_by('user_auto_id').last()
+#     if not last_user_id.user_auto_id:
+#          return 'AVI000001'
+#     user_id_no = last_user_id.user_auto_id
+#     user_id_int = int(user_id_no.split('AVI')[-1])
+#     new_user_id_int = user_id_int + 1
+#     new_user_id_no = 'AVI' + str(new_user_id_int)
+#     return new_user_id_no
     
 class Profile(models.Model):
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
@@ -35,7 +35,7 @@ class Profile(models.Model):
 		('C', 'Confirmed'),
 		('NC', 'Non-Confirmed')
 		)
-	user_auto_id = models.CharField(max_length=500, default=increment_user_id, null=True, blank=True)
+	user_auto_id = models.CharField(max_length=500, default='0000', null=True, blank=True)
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	referal_code = models.CharField(max_length=20, blank=True)
 	# sponser_id =models.OneToOneField(User, null=True, related_name='+')
