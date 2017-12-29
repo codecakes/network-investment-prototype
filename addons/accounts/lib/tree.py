c8b46949bfbadec1d1db88984065755e0bf67583
akurnya <akurnya@gmail.com>
2017-12-29 15:39:40 +0530

    fixed tree

--- addons/accounts/lib/tree.py
+++ addons/accounts/lib/tree.py
@@ -1,35 +1,48 @@
 from django.contrib.auth.models import User
 from addons.accounts.models import Profile, Members
 
-def find_left(member, member1):
-    # member.child_id.profile will give profile object 
-    # member.child. here you can pass User models filed 
-    return member if member.child_id.profile.placement_type == "L" else member1
+
+def find_left(member1, member2):
+    # member.child_id.profile will give profile object
+    return member1 if member1.child_id.profile.placement_position == "L" else member2
 
 
-def find_right(member, member1):
-    # member.child_id.profile will give profile object 
+def find_right(member1, member2):
     # member.child. here you can pass User models filed
-    return member if member.child_id.profile.placement_type == "R" else member1
+    return member1 if member1.child_id.profile.placement_position == "R" else member2
+
+
+def is_left(member):
+    return member.child_id.profile.placement_position == "L"
+
+
+def is_right(member):
+    return member.child_id.profile.placement_position == "R"
 
 
 def left_child(members):
-    # child_member = find_left(members)
-    #  abobe line will raise error for find take exactly 2 argument 1 given , need to correct
-    #  here you need to iterate over members list it could be n list 
-    child_member = find_left(*members)
-    #  just took string to check
-    return load_users(child_member.child_id)
+    if len(members) == 0: return {}
+
+    if len(members) > 1:
+        child_member = getattr(find_left(*members), 'child_id')
+    elif is_left(members[0]):
+        child_member = getattr(members[0], 'child_id')
+    else:
+        child_member = None
+    return load_users(child_member) if child_member else {}
 
 
 def right_child(members):
-    # child_member = find_left(members)
-    #  abobe line will raise error for find take exactly 2 argument 1 given , need to correct
-    #  here you need to iterate over members list it could be n list
-    # child_member = find_right(members, members)
-    child_member = find_right(*members)
-    # type error while using abobe line, check this find_left() argument after * must be an iterable, not Members
-    return load_users(child_member.child_id)
+    if len(members) == 0: return {}
+
+    if len(members) > 1:
+        child_member = getattr(find_right(*members), 'child_id')
+    elif is_right(members[0]):
+        child_member = getattr(members[0], 'child_id')
+    else:
+        child_member = None
+    print child_member
+    return load_users(child_member) if child_member else {}
 
 
 def load_users(user):
@@ -38,21 +51,22 @@ def load_users(user):
         "first_name": user.first_name,
         "last_name": user.last_name,
         "user_auto_id": profile.user_auto_id,
-        "sponsor_id": profile.sponser_id,
-        "placement_id": profile.placement_id,
+        "sponsor_id": profile.sponser_id.id,
+        "placement_id": profile.placement_id.id,
         "mobile": profile.mobile,
         "placement_position": profile.placement_position
     }
     members = Members.objects.filter(parent_id=user.id)
-    # memebers is list of child , right now i am passing only 1 member in each left or right
-    if members:
+    print "members is {}".format(members)
+    if len(members) > 0:
         return {
             'left' : left_child(members),
             'right' : right_child(members),
             'user': user_details
-        
         }
     else:
         return {
-            'user':user_details
+            'user':user_details,
+            'left': {},
+            'right': {}
         }
