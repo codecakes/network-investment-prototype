1daae46dac185b24cc9d8deb5e0d0def05c0d665
akurnya <akurnya@gmail.com>
2018-01-07 18:33:23 +0530

    changed port

--- avicrypto/settings.py
+++ avicrypto/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'USER': os.environ["avicrypto_user"],
         'PASSWORD': os.environ["avicrypto_pwd"],
         'HOST': os.environ["avicrypto_host"],
-        'PORT': '5433'
+        'PORT': '5432'
     }
 }
 
