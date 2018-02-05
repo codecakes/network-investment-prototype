760c3c3d4fcdd8e96a8f1cf3a837b07f46472853
akurnya <akurnya@gmail.com>
2018-02-06 00:51:15 +0530

    added reset once

---
+++ addons/accounts/management/commands/reset_once.py
@@ -0,0 +1,37 @@
+# runs one time
+import datetime
+from pytz import UTC
+from addons.accounts.models import User
+from addons.packages.models import User_packages
+
+from django.core.management.base import BaseCommand, CommandError
+from polls.models import Question as Poll
+
+def reset_these():    
+    for email in ["robertjohnlie@gmail.com", "erwinyap.btc@gmail.com", "lilibest.btc@gmail.com",
+        "zoulbizz@gmail.com", "andrisaputra148@gmail.com", "masta2803@gmail.com", 
+        "amirryand8@gmail.com", "suherman6224@gmail.com", "cryptomujur@gmail.com",
+        "billioner.kaya@gmail.com", "wildareflita1972@gmail.com"]:
+        u = User.objects.get(email = email)
+        u.date_joined = UTC.normialize(UTC.localize(datetime.datetime(2018, 2, 1, 00, 00, 00)))
+        u.set_password("avi123456")
+        u.save()
+        pkg = User_packages.objects.get(user = u)
+        pkg.updated_at = u.date_joined
+        pkg.last_payout_date = u.date_joined
+        pkg.binary = 0.0
+        pkg.weekly = 0.0
+        pkg.direct = 0.0
+        pkg.total_payout = 0.0
+        pkg.left_binary_cf = 0.0
+        pkg.right_binary_cf = 0.0
+        pkg.save()
+
+
+
+class Command(BaseCommand):
+    help = 'runs and resets few accounts to initial values'
+
+    def handle(self, *args, **options):
+        reset_these()
+        self.stdout.write(self.style.SUCCESS('Successfully reset'))
