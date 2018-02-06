18d8e7ea1bc3a4569be0e19aa7a03181c61f2c4c
Atul <jain.atul43@gmail.com>
2018-02-06 17:59:16 +0530

    active check decorator and crypto_account commented , some issues are there

--- avicrypto/services.py
+++ avicrypto/services.py
@@ -1,4 +1,6 @@
 import requests
+from django.contrib.auth.decorators import user_passes_test
+
 uri = 'https://api.mailgun.net/v3/avicrypto.us/messages'
 key = 'key-1055741f06d43a548bf5def6962b536a'
 
@@ -21,3 +23,16 @@ def support_mail(subject, body, receipient_emails, from_email="postmaster"):
     }
 
     return requests.post(uri, auth=("api", key), data=data)
+
+
+# def crypto_account(function):
+#     def wrapper(request, **kw):  
+#         user = request.user  
+#         if user and (user.useraccount.btc_address or user.useraccount.eth_address or (user.useraccount.xrp_address and user.useraccount.destination_tag)):
+#             kw['package_access_disable'] = False
+#         return function(request, **kw)
+#     return wrapper
+
+def active_required(function):
+    dec = user_passes_test(lambda u: u.is_active, '/not-active', '')
+    return dec(function)
