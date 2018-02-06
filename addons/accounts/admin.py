77a0563182db0812ba1a229b1d762424f1efb982
Atul <jain.atul43@gmail.com>
2018-02-06 10:12:58 +0530

    register userAccount models to admin panel

--- addons/accounts/admin.py
+++ addons/accounts/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from models import Profile, Members
+from models import Profile, Members, UserAccount
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
@@ -15,5 +15,11 @@ class MemberAdmin(admin.ModelAdmin):
 		self.list_display = [field.name for field in model._meta.fields]
 		super(MemberAdmin, self).__init__(model, admin_site)
 
+class UserAccountAdmin(admin.ModelAdmin):
+	def __init__(self, model, admin_site):
+		self.list_display = [field.name for field in model._meta.fields]
+		super(UserAccountAdmin, self).__init__(model, admin_site)
+
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Members, MemberAdmin)
+admin.site.register(UserAccount, UserAccountAdmin)
