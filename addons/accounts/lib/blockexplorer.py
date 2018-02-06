9e88dedb9ae3d12a654bf2b8746f3f904aa1fb14
akurnya <akurnya@gmail.com>
2018-02-07 04:56:44 +0530

    included relevant migrations file init. services: mailer function that emails according to crypto payment status. WIP: tests. added urls:  django redis url. added redis rq related CONFIG in settings and local settings. clock: added another scheduler for crypto payment checker. blockexplorer: users rq job worker now

--- addons/accounts/lib/blockexplorer.py
+++ addons/accounts/lib/blockexplorer.py
@@ -1,6 +1,9 @@
 from urllib2 import urlparse
 import requests
 from addons.accounts.lib.tree import divide_conquer
+import django_rq
+
+QUEUE = django_rq.get_queue('crypto_payments', autocommit=True, async=True)
 
 ETHER_KEY = "6F98VB1ZJ6MY46JHZ9AVR6YJQJPU3RCH95"
 
@@ -46,7 +49,7 @@ def is_valid_xrp_paid(amt, txn_id, addr, src_addr, payment):
     return payment.amount == amt and payment.source == src_addr and payment.destination == addr, payment.tx_hash == txn_id
 
 
-def validate_transaction(amt, src_addr, addr, txn_id, coin="btc"):
+def validate(amt, src_addr, addr, txn_id, coin="btc"):
     """Validates the given address and transaction of the given crypto payment type which can by anyone of BTC, ETH, XRP"""
     txn_api, addr_api = COIN[coin](addr, txn_id)
     txn_res = requests.get(txn_api)
@@ -60,14 +63,29 @@ def validate_transaction(amt, src_addr, addr, txn_id, coin="btc"):
                                                                                                        [each['scriptPubKey']['addresses']
                                                                                                         for each in j_txn["vout"]])
             if j_txn["confirmations"] > 6 and addr in addresses:
-                return True
+                result = True
     elif coin == "eth":
         if txn_res and addr_res:
-            return True if txn_res.get("status", None) == "1" and addr_res.get("status", None) == "1" else False
+            result = True if (txn_res.get("status", None) == "1" and addr_res.get("status", None) == "1") else False
     elif coin == "xrp":
         j_txn = txn_res.json()
         j_addr = addr_res.json()
         if j_addr.result != "error" and j_txn.result != "error":
             payments = j_txn.payments
-            return divide_conquer(payments, 0, len(payments) - 1, lambda payment: is_valid_xrp_paid(amt, txn_id, addr, src_addr, payment))
-    return False
+            result = any(divide_conquer(payments, 0, len(payments) - 1, lambda payment: is_valid_xrp_paid(amt, txn_id, addr, src_addr, payment)))
+    result = False
+    return {addr: result}
+
+
+def validate_transaction(amt, src_addr, addr, txn_id, coin="btc"):
+    """
+    RQ job worker into apscheduler
+    """
+    # QUEUE.enqueue(validate, amt, src_addr, addr, txn_id, coin=coin)
+    job = QUEUE.enqueue_call(func=validate,
+               args=(amt, src_addr, addr, txn_id),
+               kwargs={coin: coin},
+               job_id=addr)
+    job.meta['addr'] = addr
+    job.meta['amt'] = amt
+    return
