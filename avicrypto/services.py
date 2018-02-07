dd76113f4bec6074767b1538fd14fc413d9c08f4
Atul <jain.atul43@gmail.com>
2018-02-07 19:18:41 +0530

    send mail when transactions done

--- avicrypto/services.py
+++ avicrypto/services.py
@@ -1,5 +1,8 @@
 import requests
 from django.contrib.auth.decorators import user_passes_test
+from django.db.models import Q
+from addons.accounts.models import *
+from addons.packages.models import *
 
 uri = 'https://api.mailgun.net/v3/avicrypto.us/messages'
 key = 'key-1055741f06d43a548bf5def6962b536a'
@@ -16,6 +19,15 @@ def send_email_results(amt, crypto_addr, bool_result):
     3. If it was successful then set their package to active for the price `amt`
     else tell their transaction failed.
     """
+    user = UserAccount.objects.get(Q(btc_address=crypto_addr) | Q(xrp_address=crypto_addr) | Q(eth_address=crypto_addr)).user
+    if bool_result:
+        body = 'Transactions Success'
+        package = Packages.objects.get(price=amt)
+        set_package_status(user, package, 'A')
+        send_email_mailgun('AVI Crypto Transaction Success', body, user.email, from_email="postmaster")
+    else:
+        body = 'Transations failed'
+        send_email_mailgun('AVI Crypto Transaction Failed', body, user_email, from_email="postmaster")
     pass
 
 def send_email_mailgun(subject, body, receipient_emails, from_email="postmaster"):
@@ -25,7 +37,6 @@ def send_email_mailgun(subject, body, receipient_emails, from_email="postmaster"
         "subject": subject,
         "html": body
     }
-
     return requests.post(uri, auth=("api", key), data=data)
 
 def support_mail(subject, body, receipient_emails, from_email="postmaster"):
@@ -50,3 +61,8 @@ def support_mail(subject, body, receipient_emails, from_email="postmaster"):
 def active_required(function):
     dec = user_passes_test(lambda u: u.is_active, '/not-active', '')
     return dec(function)
+
+
+def set_package_status(user, package, status):
+    set_package = User_packages.objects.save(user=user, package=package, status=status)
+    return True
