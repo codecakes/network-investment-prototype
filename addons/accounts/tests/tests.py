d4dc4a848a22532fa09e11bdc37db3b961fda604
akurnya <akurnya@gmail.com>
2018-02-09 23:21:00 +0530

    checked out working settings

--- addons/accounts/tests/tests.py
+++ addons/accounts/tests/tests.py
@@ -2,5 +2,39 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-from addons.packages.lib.payout import calc
+from django.conf import settings
+from django.contrib.auth.models import User
+from addons.accounts.models import Profile, Members
+from addons.packages.models import Packages, User_packages
 
+import pytz
+import calendar
+from datetime import datetime, timedelta
+from addons.accounts.lib.tree import load_users, find_min_max, is_member_of, is_parent_of, is_valid_leg, has_child, LEG, divide_conquer, get_left, get_right
+from addons.packages.lib.payout import calc, START_TIME
+
+# TODO: Do it in time
+# class BinaryTestCase(TestCase):
+#     """Tests out binary payment"""
+    
+#     def setUp(self):
+#         self.root_user = User.objects.get(email="cryptocoin.inbox@gmail.com")
+#         self.left_user = get_left(self.root_user)
+#         self.right_user = get_right(self.root_user)
+    
+#     def test_left_user_active_pkg(self):
+#         root_pkg = User_packages.objects.get(user = self.root_user)
+#         self.assertEqual(root_pkg.package.price, 1000, "Package should be $ 1000")
+#         # self.left_user
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'testpassword'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
--- addons/accounts/tests/tests_settings.py
+++ addons/accounts/tests/tests_settings.py
@@ -1,1 +1,42 @@
 from avicrypto.local_settings import *
+# import os, urlparse
+
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# # SECURITY WARNING: keep the secret key used in production secret!
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+# TEMPLATE_DEBUG = True
+
+# # # Application definition
+
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'addons.accounts',
+#     'addons.packages',
+#     'addons.transactions',
+#     'addons.wallet',
+# ]
+# MIDDLEWARE_CLASSES = (
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# )
+# ROOT_URLCONF = 'login_test.urls'
+# WSGI_APPLICATION = 'login_test.wsgi.application'
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# TEMPLATE_DIRS = (
+#     '/home/moooeeeep/login_test/templates/',
+# )
