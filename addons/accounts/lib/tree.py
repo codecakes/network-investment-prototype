7860d48b02fd01a6edbbcd85b3b3568ecea333b9
Atul <jain.atul43@gmail.com>
2018-02-15 10:01:29 +0000

    tree.py edited online with Bitbucket
    network tree was not loading, there were some fix user id which was deleted , changed code , getting user id from query params

--- addons/accounts/lib/tree.py
+++ addons/accounts/lib/tree.py
@@ -67,8 +67,10 @@ def new_user_text(ref_code, *ref_kw):
     href = "{}&{}".format(ref_code, '&'.join(*ref_kw))
     parent = '1'
     children = '0'
-    user_auto_id = urlparse.urlparse(
-        '&pos=right&parent_placement_id=AVI000000020').path.split('=')[-1]
+    # getting parent_placement_id from href query  
+    user_auto_id = urlparse.urlparse(href).query.split('=')[-1]
+    # why this is fix ? 
+    # user_auto_id = urlparse.urlparse('&pos=right&parent_placement_id=AVI000000020').path.split('=')[-1]
     profile = Profile.objects.get(user_auto_id=user_auto_id)
     parent_user = profile.user
     # ''1' if get_left(parent_user) else '1' if get_right(parent_user) else '0'
