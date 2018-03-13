e7ada41847aca3b3b4c4653036cd1cbd004ec24d
Atul <jain.atul43@gmail.com>
2018-03-13 23:58:22 +0530

    user packages export data and transactions export data

--- addons/packages/admin.py
+++ addons/packages/admin.py
@@ -40,9 +40,9 @@ class UserPackagesAdmin(admin.ModelAdmin):
 				status = 'Active'
 			else:
 				status = 'Not-Active'
-			user_direct = calc_direct(data.user, None, None)[0]
-			user_binary = calc_binary(data.user, None, None)[0][0]
-			user_weekly = calc_weekly(data.user, None, None)[0]
+			user_direct = data.direct
+			user_binary = data.binary
+			user_weekly = data.weekly
 			total_payout = user_direct + user_binary + user_weekly
 			writer.writerow([data.id, data.user, data.user.first_name, data.user.last_name, data.user.profile.mobile, data.user.email, data.user.date_joined, data.package, data.created_at, status ,user_binary, user_direct, user_weekly, total_payout, data.paid_cur])
 		return response
--- addons/transactions/admin.py
+++ addons/transactions/admin.py
@@ -4,10 +4,61 @@ from django.contrib import admin
 from models import Transactions
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-
+from time import gmtime, strftime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import StreamingHttpResponse
+import csv
 class TransactionsAdmin(admin.ModelAdmin):
-    def __init__(self, model, admin_site):
+	actions = ['export_data_in_csv', 'export_all_in_csv']
+
+	def __init__(self, model, admin_site):
 		self.list_display = [field.name for field in model._meta.fields]
 		super(TransactionsAdmin, self).__init__(model, admin_site)
 
+	def export_data_in_csv(self, request, queryset):
+		_date = strftime("%c")
+		response = HttpResponse(content_type='text/csv')
+		response['Content-Disposition'] = 'attachment; filename="Transactions.csv"'
+		writer = csv.writer(response)
+		writer.writerow(['#DOWNLOAD-TYPE','LOCAL', 'Transactions'])
+		writer.writerow(['#DOC-TYPE','Transactions Summary'])
+		writer.writerow(["DOWNLOAD-DATE", (_date),'UTC'])
+		writer.writerow(["@DOWNLOAD-BY", 'Admin'])
+		writer.writerow([])
+		writer.writerow(["@Transactions Meta"])
+		writer.writerow(['Transations ID', 'Transactions Date', 'Sender', 'User ID','First Name', 'Last Name', 'Mobile', 'Email',  'Status', 'Transactions Type','Amount'])
+		for data in queryset:
+			if data.tx_type == 'W':
+				writer.writerow([data.tx_id, data.created_at, data.sender_wallet.owner, data.reciever_wallet.owner, data.reciever_wallet.owner.first_name, data.reciever_wallet.owner.last_name, data.reciever_wallet.owner.profile.mobile, data.reciever_wallet.owner.email, data.get_status_display(), data.get_tx_type_display(), data.amount])
+		return response
+	
+	def export_all_in_csv(self, request, queryset):
+		_date = strftime("%c")
+		response = HttpResponse(content_type='text/csv')
+		response['Content-Disposition'] = 'attachment; filename="Transactions.csv"'
+		writer = csv.writer(response)
+		writer.writerow(['#DOWNLOAD-TYPE','LOCAL', 'Transactions'])
+		writer.writerow(['#DOC-TYPE','Transactions Summary'])
+		writer.writerow(["DOWNLOAD-DATE", (_date),'UTC'])
+		writer.writerow(["@DOWNLOAD-BY", 'Admin'])
+		writer.writerow([])
+		writer.writerow(["@Transactions Meta"])
+		writer.writerow(['Transations ID', 'Transactions Date', 'Sender', 'User ID','First Name', 'Last Name', 'Mobile', 'Email',  'Status', 'Transactions Type','Amount'])
+		for data in queryset:
+			writer.writerow([data.tx_id, data.created_at, data.sender_wallet.owner, data.reciever_wallet.owner, data.reciever_wallet.owner.first_name, data.reciever_wallet.owner.last_name, data.reciever_wallet.owner.profile.mobile, data.reciever_wallet.owner.email, data.get_status_display(), data.get_tx_type_display(), data.amount])
+		return response
+
+	export_data_in_csv.short_description = 'Export Withdraw Data'
+	export_all_in_csv.short_description = 'Export All Data'
+
 admin.site.register(Transactions, TransactionsAdmin)
+
+# 	type_choices = (
+# 		('W', 'Withdraw'),
+# 		('P', 'Add Package'),
+# 		('U', 'User to User'),
+# 		('topup', 'Top-up'),
+# 		('roi', 'ROI'),
+# 		('binary', 'Binary'),
+# 		('direct', 'Direct'),
+# 	)
