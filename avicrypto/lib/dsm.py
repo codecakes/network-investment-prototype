ab85816eabcb168e338ffec531f65343771ce898
akurnya <akurnya@gmail.com>
2018-03-15 20:09:18 +0530

    reset once now populates data for all users. views exclude binary.py shit. payout revamped to state machine and time stamped transaction based payout generation.

--- avicrypto/lib/dsm.py
+++ avicrypto/lib/dsm.py
@@ -1,6 +1,9 @@
+import pdb
+
 class StateMachine(object):
     def __init__(self, user):
         self.handlers = {}
+        self.handler = None
         self.user = user
         self.start_state = None
         self.end_states = []
@@ -16,12 +19,18 @@ class StateMachine(object):
 
     def set_start(self, investment_type):
         self.start_state = investment_type
-
-    def run(self, last_date, next_date):
+    
+    def handle(self, *args, **kw):
+        if not self.handler:
+            raise Exception("No Function handler called")
+        return self.handler(self.user, *args, **kw)
+        
+    
+    def run(self, *args, **kw):
         state = self.start_state
         # print("starting state ", state)
         try:
-            handler = self.handlers[state]
+            self.handler = self.handlers[state]
             # print "new handler is ", handler
         except:
             raise Exception("must call .set_start() before .run()")
@@ -29,11 +38,20 @@ class StateMachine(object):
             raise Exception("at least one state must be an end_state")
 
         while True:
-            (returns, new_state) = handler(self.user, last_date, next_date)
+            # (returns, new_state) = handler(self.user, last_date, next_date)
+            (returns, new_state) = self.handle(*args, **kw)
+            print "Running state"
+            pdb.pprint.pprint(self.handler.__name__, depth=2)
             self.results[state] = returns
             state = new_state
             if state in self.end_states:
                 # print("reached ", new_state)
                 break
             else:
-                handler = self.handlers[state]
+                try:
+                    self.handler = self.handlers[state]
+                except KeyError as e:
+                    print "got {}. Switching to {}'s wet state".format(e.message, state)
+                    handler_state = "{}_wet".format(state)
+                    print "swtiched to %s" %handler_state
+                    self.handler = self.handlers[handler_state]
